316c267244c2b721ae160c1bbb60d13f8ba71915
Benjamin Anderson <andersonbcdefg@gmail.com>
2024-11-16 14:16:32 -0800

    some pictures/screenshots

--- gptemail.js
+++ gptemail.js
@@ -1,6 +1,4 @@
-console.log("GPTemail is running in this tab!")
 browser.runtime.onMessage.addListener((message) => {
-    console.log("got a message")
     if (message.command === "get_selection") {
         let selection = document.getSelection()
         if (selection.toString()) {
--- popup/popup.js
+++ popup/popup.js
@@ -4,8 +4,6 @@ let selection = document.querySelector("#selectedtext");
 let instructions = document.querySelector("#instructions");
 let apikeyfield = document.querySelector("#apikey");
 let userfield = document.querySelector("#user");
-let cohere = document.querySelector("#cohere");
-let openai = document.querySelector("#openai");
 
 function toast(message, duration=2500, error=false) {
     let toast = document.querySelector("#message");
@@ -24,14 +22,9 @@ function toast(message, duration=2500, error=false) {
 }
 
 function getSavedData() {
-    browser.storage.local.get(["user", "provider", "apikey"]).then(({user, provider, apikey}) => {
+    browser.storage.local.get(["user", "apikey"]).then(({user, apikey}) => {
         if (user) {
             userfield.value = user;
-        }
-        if (provider && provider === "cohere") {
-            cohere.checked = true;
-        } else {
-            openai.checked = true;
         }
         if (apikey) {
             apikeyfield.value = apikey;
@@ -52,23 +45,29 @@ async function getSelection(message) {
 }
 
 function createPrompt(formObj) {
-    let prompt = `INSTRUCTIONS: Write an email reply (from ${formObj.user}) to the message below.`
+    let messages = [];
+    messages.push(
+        {"role": "system", "content": `Please write a professional email reply on behalf of your user, ${formObj.user}. The user, ${formObj.user} will provide you with a message to reply to, and may provide additional instructions related to tone or specific details to include. You will write a response to the email on behalf the user, ${formObj.user}.`}
+    )
+    messages.push(
+        {"role":"user", "content": "EMAIL CONTENTS: " + formObj.selectedtext}
+    )
     if (formObj.instructions) {
-        prompt += ` ${formObj.instructions}`
+        messages.push(
+            {"role":"user", "content": "ADDITIONAL INSTRUCTIONS: " + formObj.instructions}
+        )
     }
-    prompt += `\n\nMESSAGE: ${formObj.selectedtext}`
-    prompt += `\n\nRESPONSE:`
-    return prompt;
+    return messages;
 }
 
-async function submitAPIRequest(provider, api_key, model, prompt) {
+async function submitAPIRequest(api_key, messages) {
     let request_url;
     
     const body = {
-        "model": model, 
-        "prompt": prompt, 
+        "model": "gpt-3.5-turbo", 
+        "messages": messages, 
         "temperature": 0.5, 
-        "max_tokens": 225
+        "max_tokens": 350
     };
 
     let headers = {
@@ -76,12 +75,7 @@ async function submitAPIRequest(provider, api_key, model, prompt) {
         'Authorization': 'Bearer ' + api_key
     };
 
-    if (provider === "cohere") {
-        headers['Cohere-Version'] = "2022-12-06";
-        request_url = "https://api.cohere.ai/generate";
-    } else {
-        request_url = "https://api.openai.com/v1/completions";
-    }
+    request_url = "https://api.openai.com/v1/chat/completions";
 
     const requestOptions = {
         method: 'POST',
@@ -94,11 +88,7 @@ async function submitAPIRequest(provider, api_key, model, prompt) {
     console.log("Status", res.status);
     if (res.ok) {
         let json = await res.json();
-        if (provider === "cohere") {
-            return json.generations[0].text;
-        } else {
-            return json.choices[0].text;
-        }
+        return json.choices[0].message.content.trim();
         
     } else {
         toast("API request failed. Check your API key and try again.", 2500, true);
@@ -121,20 +111,19 @@ function submitForm() {
         return false;
     }
 
-    // Cache user, provider, and API key
+    // Cache user and API key
     browser.storage.local.set({
         "user": formObj.user,
-        "provider": formObj.provider,
         "apikey": formObj.apikey
     });
 
     // Create prompt & select model
-    let prompt = createPrompt(formObj);
-    let model = formObj.provider === "openai" ? "text-davinci-003" : "xlarge-20221108";
+    let messages = createPrompt(formObj);
+    let model = "gpt-3.5-turbo"
     toast("Generating response...", 2500);
     
     // Get response from LLM API
-    submitAPIRequest(formObj.provider, formObj.apikey, model, prompt).then((response) => {
+    submitAPIRequest(formObj.apikey, messages).then((response) => {
         if (response) {
             result.value = response.trim();
             navigator.clipboard.writeText(response.trim());
